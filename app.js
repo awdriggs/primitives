ada4b43e1f178087d6e57da8931d76d4dd525ad7
adam driggers <awdriggs@gmail.com>
2016-12-14 22:57:23 -0500

    testing switch

--- app.js
+++ app.js
@@ -1,1 +1,45 @@
 //document.getElementById('myCanvas').dataset.processingSources
+var index = 1;
+var numSketches = 7; //upper limit, set to the highest number sketch that you have in the file.
+function next(){
+    console.log('poop');
+  index++;
+  updateSketch();
+}
+
+function prev(){
+  console.log('piss');
+  index--;
+  updateSketch();
+}
+
+function updateButtons(){
+}
+
+/*
+var sketches = ["sketchname1", "example", "test"];
+function nextSketch(){
+              var currentLoc = $.inArray($('#canvas').attr('name'), sketches);
+              currentLoc ++;
+              if(currentLoc >= sketches.length){
+                              currentLoc = 0;
+                            }
+              var htmlStr = '<canvas id="canvas" name="' + sketches[currentLoc] + '" data-processing-sources="' + sketches[currentLoc] + '.pde"></canvas>';
+              console.log(htmlStr);
+      $('#canvas').replaceWith(htmlStr);
+      Processing.reload();
+}
+
+*/
+
+function updateSketch(){
+  var main = document.getElementsByTagName('main')[0];
+  main.innerHTML = ''; //clear out the current canvas
+
+  var testStr = "<canvas id='myCanvas' data-processing-sources='assets/primitive"+index+".pde'></canvas>";
+  console.log(testStr);
+  main.innerHTML = testStr;
+  Processing.reload();
+}
+
+
