4c58dda9fa1cb25be813bb4dcc07e85ae901e933
adam driggers <awdriggs@gmail.com>
2016-12-17 00:24:05 -0500

    on/off buttons

--- app.js
+++ app.js
@@ -1,45 +1,74 @@
 //document.getElementById('myCanvas').dataset.processingSources
 var index = 1;
 var numSketches = 7; //upper limit, set to the highest number sketch that you have in the file.
-function next(){
-    console.log('poop');
+
+function next() {
+  console.log('poop');
   index++;
   updateSketch();
 }
 
-function prev(){
+function prev() {
   console.log('piss');
   index--;
   updateSketch();
 }
 
-function updateButtons(){
+
+function updateButtons() {
+  //if current index is great than 1, then the - button should be on
+  //  add the button listener
+  //  remove the off class from the button, add the on class
+  //else the minus button should be off
+  //  remove the on class from the button
+  //  add the off class
+  //  remove the button listener
+  var prevButton = document.getElementById("prev")
+
+  if (index > 1) {
+    prevButton.addEventListener("click", prev);
+    prevButton.classList.remove('off');
+    prevButton.classList.add('on');
+  } else {
+    prevButton.removeEventListener("click", prev);
+    prevButton.classList.remove('on');
+    prevButton.classList.add('off');
+  }
+
+  var nextButton = document.getElementById("next");
+
+  if(index < numSketches) {
+    nextButton.addEventListener("click", next);
+    nextButton.classList.remove("off")
+    nextButton.classList.add("on");
+  } else {
+    nextButton.classList.remove("on");
+    nextButton.classList.add("off");
+    nextButton.removeEventListener("click", next);
+  }
+
+  //if the current index is less than or equal to the number of sketches,
+  //  add the button listener
+  //  remove the off class
+  //  add the on class ot the ubtton
+  //else 
+  //  remove the on class
+  //  add the off class
+  //  remove the button listener
 }
 
-/*
-var sketches = ["sketchname1", "example", "test"];
-function nextSketch(){
-              var currentLoc = $.inArray($('#canvas').attr('name'), sketches);
-              currentLoc ++;
-              if(currentLoc >= sketches.length){
-                              currentLoc = 0;
-                            }
-              var htmlStr = '<canvas id="canvas" name="' + sketches[currentLoc] + '" data-processing-sources="' + sketches[currentLoc] + '.pde"></canvas>';
-              console.log(htmlStr);
-      $('#canvas').replaceWith(htmlStr);
-      Processing.reload();
-}
-
-*/
-
-function updateSketch(){
+function updateSketch() {
   var canvasDiv = document.getElementById('canvas-wrapper');
   canvasDiv.innerHTML = ''; //clear out the current canvas
 
-  var testStr = "<canvas id='myCanvas' data-processing-sources='assets/primitive"+index+".pde'></canvas>";
+  var testStr =
+    "<canvas id='myCanvas' data-processing-sources='assets/primitive" + index +
+    ".pde'></canvas>";
   console.log(testStr);
   canvasDiv.innerHTML = testStr;
   Processing.reload();
+
+  updateButtons(); 
 }
 
-
+updateButtons();
